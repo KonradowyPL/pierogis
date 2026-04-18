28cd72b94854aeec9879698d563ba1b7534ff77f
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-04-18 18:23:30 +0200

    set water level

--- src/main/java/pl/konradowy/pierogis/mixins/BasinMixin.java
+++ src/main/java/pl/konradowy/pierogis/mixins/BasinMixin.java
@@ -16,6 +16,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import net.minecraft.nbt.CompoundTag;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+
 @Mixin(BasinBlockEntity.class)
 public class BasinMixin {
 	@Shadow
@@ -23,21 +27,74 @@ public class BasinMixin {
 	@Shadow
 	public SmartFluidTankBehaviour outputTank;
 
+	long lastTick = 0;
+
 	@Inject(method = "tick", at = @At("TAIL"))
 	private void onTick(CallbackInfo ci) {
-		System.err.println("BASIN TICK");
-		Level level = ((BlockEntity) (Object) this).getLevel();
-		BlockPos pos = ((BlockEntity) (Object) this).getBlockPos();
+		BlockEntity be  = ((BlockEntity) (Object) this);
+		Level level = be.getLevel();
+		long ticks = level.getGameTime();
+
+		long delta = ticks - lastTick;
+		lastTick = ticks;
 
 		if (level.isClientSide)
 			return;
 
-		System.err.println(pos);
-		BlockEntity blockEntity = level.getBlockEntity(pos);
-		CompoundTag tag = blockEntity.saveWithFullMetadata(level.registryAccess());
-		System.err.println(tag);
+		BlockPos pos = ((BlockEntity) (Object) this).getBlockPos();
+		CompoundTag nbt = be.saveWithFullMetadata(level.registryAccess());
+		// CompoundTag fluid = getFirstInputTankFluid(tag);
 
-		// System.err.println(this.);
+		// if (fluid == null)
+		// return;
+
+		// String fluidId = fluid.getString("id");
+		// int amount = fluid.getInt("amount");
+		// System.err.println(fluidId);
+		// System.err.println(amount);
+		ListTag inputTanks = nbt.getList("InputTanks", Tag.TAG_COMPOUND);
+		if (inputTanks.isEmpty())
+			return;
+
+		CompoundTag firstTank = inputTanks.getCompound(0);
+		CompoundTag tankContent = firstTank.getCompound("TankContent");
+
+		if (tankContent.contains("Fluid")) {
+			CompoundTag fluid = tankContent.getCompound("Fluid");
+			String fluidId = fluid.getString("id");
+			int amount = fluid.getInt("amount");
+			System.err.println(fluidId);
+			System.err.println(amount);
+
+			fluid.putInt("amount", 100);
+
+			tankContent.put("Fluid", fluid);
+			firstTank.put("TankContent", tankContent);
+			inputTanks.set(0, firstTank);
+
+			nbt.put("InputTanks", inputTanks);
+
+			be.loadWithComponents(nbt, level.registryAccess());
+			be.setChanged();
+			level.sendBlockUpdated(pos, be.getBlockState(), be.getBlockState(), 3);
+		}
 
 	}
+
+	// private static CompoundTag getFirstInputTankFluid(CompoundTag nbt) {
+
+	// // InputTanks is a list
+	// ListTag inputTanks = nbt.getList("InputTanks", Tag.TAG_COMPOUND);
+
+	// if (inputTanks.isEmpty()) {
+	// return null;
+	// }
+
+	// CompoundTag firstTank = inputTanks.getCompound(0);
+	// CompoundTag tankContent = firstTank.getCompound("TankContent");
+	// if (!tankContent.contains("Fluid")) {
+	// return null;
+	// }
+	// return tankContent.getCompound("Fluid");
+	// }
 }
