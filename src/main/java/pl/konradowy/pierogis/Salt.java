a604c807e92afb9089487864a96414c3df249118
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-05-01 15:34:38 +0200

    nie umiem się zdecydować jak nazwać sól

--- src/main/java/pl/konradowy/pierogis/Salt.java
+++ src/main/java/pl/konradowy/pierogis/Salt.java
@@ -1,17 +1,36 @@
 package pl.konradowy.pierogis;
 
 import java.util.List;
+import java.util.Random;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-
 public class Salt extends Item {
+  private String name;
+
   public Salt(Properties properties) {
     super(properties);
+    String[] names = { "chlorek sodu",
+        "sodek chloru",
+        "NaCl",
+        "sól kuchenna",
+        "sól spożywcza",
+        "sól jadalna",
+        "sól stołowa",
+        "sól warzona",
+        "sól kopalniana",
+        "sól.mp4",
+        "sól sodowa",
+        "soda chlorkowa",
+        "chlorek sodowy",
+        "sodium chloridum",
+        "sul",
+        "to coś co dodaje się do pierogów", };
+    name = names[new Random().nextInt(names.length)];
   }
 
   @Override
@@ -22,5 +41,11 @@ public class Salt extends Item {
 
     tooltip.add(Component.translatable("tooltip.pierogis.salt")
         .withStyle(ChatFormatting.GRAY));
+  }
+
+  @Override
+  public Component getName(ItemStack stack) {
+
+    return Component.literal(name);
   }
 }
